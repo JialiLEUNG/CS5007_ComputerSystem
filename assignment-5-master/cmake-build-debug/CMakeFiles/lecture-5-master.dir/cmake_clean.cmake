85c8ba444db90347a1e4f77fe7a77bd38027b025
JialiLEUNG <47040009+JialiLEUNG@users.noreply.github.com>
2019-06-21 11:23:04 -0700

    Add files via upload

---
+++ assignment-5-master/cmake-build-debug/CMakeFiles/lecture-5-master.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lecture-5-master.dir/assignment-5.c.o"
+  "lecture-5-master.pdb"
+  "lecture-5-master"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lecture-5-master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
